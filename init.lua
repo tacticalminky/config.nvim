271bd0d78227ca6b11e4e12c33d30249c0a12104
TacticalMinky <mink91094@gmail.com>
2023-10-02 22:04:10 -0500

    edit

--- init.lua
+++ init.lua
@@ -1,8 +1,5 @@
 -- init.lua
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 require('base.remaps')
 require('base.sets')
 
