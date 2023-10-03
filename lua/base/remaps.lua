9ad1af78c316de9732b41db2af8f55a214b02363
TacticalMinky <mink91094@gmail.com>
2023-10-02 22:03:41 -0500

    edit

--- lua/base/remaps.lua
+++ lua/base/remaps.lua
@@ -1,5 +1,8 @@
 -- remaps.lua
 
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
